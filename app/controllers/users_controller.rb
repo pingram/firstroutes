d86585bf39c4ab270b62f5631b61ccf65f2e9baf
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-22 11:56:41 -0700

    finished user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,36 @@
 class UsersController < ApplicationController
+
   def index
-    render :json => params
+    @users = User.all
+    render :json => @users
   end
 
   def create
-    # render :text => "posted to create!"
-    render :json => params[:name]
+    user = User.new(params[:user].permit(:username, :email))
+    # in Rails 4 you must whitelist attributes with #permit
+    if user.save
+      render :json => user
+    else
+      render :json => user.errors.full_messages, :status => :unprocessable_entity
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+    render :json => @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params.require(:user).permit(:name, :email))
+    @user.save!
+    render :json => @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.try :destroy
+    render json: { message: "user was destroyed"}
+  end
+
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -5,14 +5,27 @@ url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users.json',
-  query_values: {
-      'person[age]' => 200,
-      'person[name]' => 'bob',
-      'person[last_name]' => 'jenkins',
-      'person[likes][cats]' => 'gizmo'
-  }
+  path: '/users/1.json',
+  # query_values:
+
 ).to_s
+p url
 #   vals = {:name => 'bob'
 
-puts RestClient.get(url)
+# puts RestClient.get(url)
+
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/2.json'
+  ).to_s
+
+  puts RestClient.delete(
+    url,
+    { :user => { :name => "Steve"} }
+  )
+end
+
+create_user
