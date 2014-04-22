b58fc8f336ba35e1bb77b41f916edf3ef6565d0a
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-22 10:54:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstroutes::Application.config.secret_key_base = '4f3d52e9a7e54652108f827721da2a38309c18782027a01d2277a98b9e418e1a429e7488eb2e7585fc9d1857bd793b8554eb10c6187786c032bae59bf9bf1019'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firstroutes::Application.config.session_store :cookie_store, key: '_firstroutes_session'
